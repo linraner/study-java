40c50f71360bff87b67e6742a8f118e357dea6d5
linran <linran@live.com>
2019-10-04 10:33:14 +0800

    CountDownLatch

--- java-jdk/src/main/java/com/lin/juc/countdownlatch/Main.java
+++ java-jdk/src/main/java/com/lin/juc/countdownlatch/Main.java
@@ -10,38 +10,19 @@ import java.util.concurrent.CountDownLatch;
  **/
 public class Main {
     public static void main(String[] args) {
-//        Stopwatch stopwatch = Stopwatch.createStarted();
-//        stopwatch.start();
-//        // 创建一个初始值为5的倒数计数器
-//        CountDownLatch countDownLatch = new CountDownLatch(5);
-//        for (int i = 0; i < 5; i++) {
-//            Thread thread = new TestThread(countDownLatch);
-//            thread.start();
-//        }
-//        try {
-//            // 阻塞当前线程，直到倒数计数器倒数到0
-//            countDownLatch.await();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//        stopwatch.stop();
-//        System.out.println("子线程执行时长：" + stopwatch.elapsed());
         long start = System.currentTimeMillis();
-
         // 创建一个初始值为5的倒数计数器
         CountDownLatch countDownLatch = new CountDownLatch(5);
         for (int i = 0; i < 5; i++) {
             Thread thread = new TestThread(countDownLatch);
             thread.start();
         }
-
         try {
             // 阻塞当前线程，直到倒数计数器倒数到0
             countDownLatch.await();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
         long end = System.currentTimeMillis();
         System.out.println("子线程执行时长：" + (end - start));
     }
