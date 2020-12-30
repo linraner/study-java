82dd800be9b527117cb5dfa077e00a4614a9524e
baize <linran@live.com>
2020-12-30 10:20:37 +0800

    .

---
+++ java-jdk/src/main/java/com/lin/utils/FileUtils.java
@@ -0,0 +1,12 @@
+package com.lin.utils;
+
+import java.util.Objects;
+
+public class FileUtils {
+
+
+  public static void main(String[] args) {
+    System.out.println(Objects.requireNonNull(FileUtils.class.getClassLoader().getResource("`pwd`")).getPath());;
+  }
+
+}
