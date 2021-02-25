8c09199cc684e3e0795b6da88d7a281b7e98646d
baize <linran@live.com>
2021-02-25 12:10:06 +0800

    .

--- java-jdk/src/main/java/com/lin/basic/Main.java
+++ java-jdk/src/main/java/com/lin/basic/Main.java
@@ -13,22 +13,21 @@ public class Main {
   public static void main(String[] args) throws InterruptedException {
 //    testCall();
 
-    testWhile(false);
+    testWhile(true);
 
   }
 
   static void testWhile(boolean running) throws InterruptedException {
-    while (true) {
+    while (running) {
       while (running) {
-        while (running) {
-          Thread.sleep(1000);
-          System.out.println("test 1");
-          running = false;
-        }
-        Thread.sleep(2000);
+        Thread.sleep(1000);
+        System.out.println("test 1");
         running = false;
-        System.out.println("test 2");
       }
+      Thread.sleep(2000);
+      System.out.println("test 2");
+      Thread.sleep(1000);
+      running = true;
     }
   }
 
