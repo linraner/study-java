96a73f5c1dd268791e44ad79704b0373274ceebe
baize <linran@live.com>
2021-03-08 11:50:20 +0800

    .

--- java-jdk/src/main/java/com/lin/basic/Main.java
+++ java-jdk/src/main/java/com/lin/basic/Main.java
@@ -1,9 +1,18 @@
 package com.lin.basic;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public class Main {
 
@@ -13,8 +22,9 @@ public class Main {
   public static void main(String[] args) throws InterruptedException {
 //    testCall();
 
-    testWhile(true);
+//    testWhile(true);
 
+    testExecService();
   }
 
   static void testWhile(boolean running) throws InterruptedException {
@@ -28,6 +38,45 @@ public class Main {
       System.out.println("test 2");
       Thread.sleep(1000);
       running = true;
+    }
+  }
+
+  static void testExecService() {
+    ThreadPoolExecutor poolExecutor = new ThreadPoolExecutor(
+        1,
+        1,
+        0,
+        TimeUnit.SECONDS,
+        new ArrayBlockingQueue<>(1),
+        new ThreadFactoryBuilder().setNameFormat("test" + "-%d").build(),
+        new ThreadPoolExecutor.DiscardPolicy());
+
+    List<Future<?>> futures = new ArrayList<>(10);
+    for (int i = 0; i < 10; i++) {
+      Future<?> f = poolExecutor.submit(() -> testTask());
+      futures.add(f);
+    }
+
+    futures.forEach(x -> {
+      try {
+        System.out.println(x.get(100, TimeUnit.MILLISECONDS));
+      } catch (InterruptedException | ExecutionException | TimeoutException e) {
+        System.out.println("time out");
+      }
+    });
+
+    System.out.println("exec end");
+
+  }
+
+  static String testTask() {
+    System.out.println("exec ing");
+    try {
+      Thread.sleep(100);
+      return "test";
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+      return "except";
     }
   }
 
