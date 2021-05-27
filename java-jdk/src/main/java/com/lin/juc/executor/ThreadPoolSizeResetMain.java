001549f8d2a9fc20909555f79d8d1b4905e7f705
linran <linran@live.com>
2021-05-27 11:20:03 +0800

    feat: add thread pool reset size

--- java-jdk/src/main/java/com/lin/juc/executor/ThreadPoolSizeResetMain.java
+++ java-jdk/src/main/java/com/lin/juc/executor/ThreadPoolSizeResetMain.java
@@ -1,5 +1,6 @@
 package com.lin.juc.executor;
 
+import java.time.LocalTime;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -10,7 +11,7 @@ public class ThreadPoolSizeResetMain {
   private static final int CORE_SIZE = 4;
   private static final int MAX_SIZE = 4;
 
-  private static final Semaphore semaphore = new Semaphore(CORE_SIZE);
+  private static final Semaphore semaphore = new Semaphore(CORE_SIZE * 10);
 
   private static final ThreadPoolExecutor executor = new ThreadPoolExecutor(CORE_SIZE, MAX_SIZE, 1000, TimeUnit.SECONDS, new LinkedBlockingDeque<>());
 
@@ -19,12 +20,16 @@ public class ThreadPoolSizeResetMain {
     for (int i = 0; i < Integer.MAX_VALUE; i++) {
       semaphore.acquire();
       executor.submit(ThreadPoolSizeResetMain::handle);
+      if (i == 10) {
+        resetThreadPoolSize(6);
+      }
     }
   }
 
   private static void handle() {
     try {
       Thread.sleep(1000);
+      System.out.println(LocalTime.now().toString());
     } catch (InterruptedException e) {
       e.printStackTrace();
     } finally {
